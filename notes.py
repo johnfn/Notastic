07d575501dda9be781febfd21b7ae549b92120c2
johnfn <johnfn@gmail.com>
2011-06-28 08:45:51 -0700

    Save notes on every keystroke.
    It saves in a really stupid way (delete whole file, recreate whole file)
    but it works fine enough. The right way (of course) is to just do a diff
    but it just doesn't seem worth it.

--- notes.py
+++ notes.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python
 
+# TODO C-X quit
+
 from Tkinter import *
 import os.path
 import pickle
 
+DIR = "/home/grant/notes/"
+
 class Internal:
   """ Internal class to Settings. """
+
   def __init__(self):
-    self.last_edited = "main"
+    self.current_file = "main"
     self.all_files = ["main"]
 
 class Settings:
+  """ State is kept between different sessions by pickling/unpickling the 
+  Settings object (or, to be more precise, the Internal object). """
+
   SAVE_FILE = "notes.settings"
   internal = None
 
@@ -18,18 +26,23 @@ class Settings:
     self.load()
 
   def add_file(self, file_name):
+    print "loading %s" % file_name
     self.internal.all_files.append(file_name)
 
   def get_text(self):
-    return "\n".join([line for line in open(self.internal.last_edited)])
+    return "\n".join([line for line in open(DIR + self.internal.current_file)])
+
+  def get_file(self):
+    return self.internal.current_file
 
   def save(self):
-    pickle.dump(self.internal, open(self.SAVE_FILE, 'wb'))
+    """ Save the settings. """
+    pickle.dump(self.internal, open(DIR + self.SAVE_FILE, 'wb'))
 
   def load(self):
     if os.path.exists(self.SAVE_FILE):
-      self.internal = pickle.load(open(self.SAVE_FILE, 'rb'))
-      print self.internal.last_edited
+      self.internal = pickle.load(open(DIR + self.SAVE_FILE, 'rb'))
+      print self.internal.current_file
     else:
       self.internal = Internal()
 
@@ -50,6 +63,24 @@ class Notes:
     self.text.focus_set()
 
     self.text.insert(END, self.settings.get_text())
+
+  # Shows the textbox prompt.
+  def prompt_user(self):
+    self.input_txt.place(x = 0, y = 20)
+    self.text.focus_set()
+
+  # Eventually I should abstract out all text functionality, etc...
+  def get_text(self):
+    return self.text.get(1.0, END)
+
+  def save_content(self):
+    file_path = DIR + self.settings.get_file()
+    if not os.path.exists(file_path):
+      os.remove(file_path)
+
+    file_data = open(file_path, 'w')
+    file_data.writelines(self.get_text())
+    root.wm_title("herrp")
 	
   def change_text(self, event):
     key = -1
@@ -58,7 +89,9 @@ class Notes:
       key = ord(event.char)
 
     if key == 6: # C-F
-      print "ok"
+      self.prompt_user()
+    else:
+      self.save_content()
 
     """
     print event
@@ -84,6 +117,10 @@ class Notes:
     scroll.pack(side=RIGHT,fill=Y)
     textfr.pack(side=TOP)
 
+    self.input_txt = Text(textfr, height=2, width=50, background = 'white')
+    self.input_txt.place(x = 0, y = 20)
+    self.input_txt.pack()
+    self.input_txt.pack_forget()
 
 def main():
   root = Tk()
